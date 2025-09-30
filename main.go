02da8e9d172fb7774c7691f4cd13dbde842c9d59
酒破儿 <154944277+loecom@users.noreply.github.com>
2025-09-30 16:00:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -597,6 +597,15 @@ func getIndex() string {
         <div class="advantages">
             <h2>回家核心优势</h2>
             <ul>
+			    <!--
+                <li><strong>永久免费：</strong>还有什么东西能比免费更好。</li>
+                <li><strong>配置简单：</strong>设备要求低，配置简单。</li>
+                <li><strong>无需工具：</strong>WEB应用直接浏览器访问，客户端无需工具。</li>
+                <li><strong>高速直联：</strong>STUN内网穿透，免服务器中转点对点更高效。</li>
+                <li><strong>固定地址：</strong>穿透地址永不过期，也不需要频繁验证。</li>
+                <li><strong>跳转加密：</strong>可加入验证密码，提升部分应用安全性。</li>
+                <li><strong>携带后缀：</strong>支持携带后缀跳转，无公网IP也能分享文件。</li>
+				-->
             </ul>
         </div>
     </div>
@@ -807,6 +816,7 @@ func getRegisterHtml() string {
 </body>
 </html>`
 }
+
 
 
 
